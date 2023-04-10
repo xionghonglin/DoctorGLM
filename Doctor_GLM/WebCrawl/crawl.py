8a640ceb9db46eaeae64898341e53b845c3a7a81
VeryGreatGuy <60505198+zhaozh10@users.noreply.github.com>
2023-04-10 22:50:00 +0800

    clean crawl.py

--- Doctor_GLM/WebCrawl/crawl.py
+++ Doctor_GLM/WebCrawl/crawl.py
@@ -17,7 +17,6 @@ def ret_website(query:str):
 
 def is_treatment(tag:PageElement,feature: str):
     return tag.name == 'h2' and tag.has_attr('class') and 'topic__header--section' in tag['class'] and feature in tag.get_text(strip=True)
-    # return tag.name == 'h2' and tag.has_attr('class') and 'topic__header--section' in tag['class'] and tag.get_text(strip=True) == feature
 
 def preclean(content:str):
     res = re.sub(r'\s+', '', content)
@@ -25,8 +24,6 @@ def preclean(content:str):
 
 def text2subsec(content_elem:PageElement):
     head = content_elem.find_all('h3', attrs='topic__header--subsection')
-    # head = content_elem.find_next_siblings('h3', attrs='topic__header--subsection')
-    # head=content_elem.find_all_next('section', class_='topic__section GHead')
     if len(head) == 0:
         return head
     if '参考文献' in head[-1].text:
@@ -38,8 +35,6 @@ def subsec2dict(content_elem:PageElement,sec:list):
     intro_list = []
     # 如果没有subsection则直接返回字典
     if len(sec)==0:
-        # 是否过滤href的内容
-        # info["开头"] = preclean(rm_link(content_elem))
         primer=content_elem.get_text()
         primer = re.sub(r'\s+', ' ', primer)
         info["开头"] = primer
@@ -67,14 +62,8 @@ def subsec2dict(content_elem:PageElement,sec:list):
     return info
 
 def parser(soup,feature:str):
-    # 查找data-originaltitle="治疗"的标签
+    # 查找包含指定关键字的Tag
     treatment_tag=soup.find_all(lambda tag: is_treatment(tag, feature))
-    # treatment_tag = soup.find_all(attrs={"data-originaltitle": feature})
-    # # head = soup.find_all('h2', attrs='topic__header--section')
-    # # treatment_tag=soup.find('section', class_='topic__section FHead')
-    # if not treatment_tag:
-    #     # head = soup.find_all('h2', attrs='topic__header--section')
-    #     treatment_tag=soup.find_all(lambda tag: is_treatment(tag, feature))
     # 检查是否找到了符合条件的标签
     if treatment_tag:
         # 获取紧跟在目标标签后面的div标签
@@ -91,8 +80,6 @@ def parser(soup,feature:str):
 def file_to_4_attr(filename:str):
     # 症状和体征 & 诊断 & 预后 & 治疗
     info=[]
-    # 该features出于爬虫目的设计
-    # 保存为字典时请记录为["症状和体征","诊断","预后","治疗"]
     features=["症状","诊断","预后","治疗"]
     soup = BeautifulSoup(open(filename,encoding='utf-8').read(), 'html.parser')
     for f in features:
@@ -104,10 +91,6 @@ def file_to_4_attr(filename:str):
 
 if __name__=="__main__":
     # 返回 症状和体征 & 诊断 & 预后 & 治疗 和对应的subsection
-    # 二尖瓣狭窄： ../MSD/{D2F11D5E-75DD-43D9-9932-5EC561FCCC2E}.html
-    # 支原体感染：../MSD/{2B5089C6-BFCF-4649-BBC9-6A0D8A9DB525}.html
-    # 高血压：../MSD/{6DE49BCA-DE4B-4666-B0A2-49FC9C8F357A}.html
-    # 肛门直肠痿：../ MSD/{D7E4E1ED-E2C8-4418-A337-915596F5A756}.html
     query_link={}
     log_step=100
     disease_info={}
